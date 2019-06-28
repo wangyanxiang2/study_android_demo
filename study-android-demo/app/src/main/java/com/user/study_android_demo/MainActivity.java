a83ebe4aa0ae135a9bf30a4c57e7dc161cce7e4b
wangyanxiang2 <wangyanxiang2@hisense.com>
2019-06-28 14:11:07 +0800

    button listener 采用匿名类的写法

--- study-android-demo/app/src/main/java/com/user/study_android_demo/MainActivity.java
+++ study-android-demo/app/src/main/java/com/user/study_android_demo/MainActivity.java
@@ -2,12 +2,31 @@ package com.user.study_android_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean ledon = false;
+    private Button button =null ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.BUTTON);
+        //匿名类写法
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                ledon =!ledon;
+                if(ledon)
+                    button.setText("ALL OFF");
+                else
+                    button.setText("ALL ON");
+            }
+        });
+
+
     }
 }
