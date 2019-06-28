19074c1a09e4d32d7f60b211e93e4d2f773f41f5
wangyanxiang2 <wangyanxiang2@hisense.com>
2019-06-28 15:27:28 +0800

    添加button和checkbox监听方法

--- study-android-demo/app/src/main/java/com/user/study_android_demo/MainActivity.java
+++ study-android-demo/app/src/main/java/com/user/study_android_demo/MainActivity.java
@@ -2,31 +2,70 @@ package com.user.study_android_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItemView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean ledon = false;
     private Button button =null ;
+    private CheckBox checkBoxLed1 = null;
+    private CheckBox checkBoxLed2 = null;
+
+    class myButtonListener implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View view) {
+            ledon =!ledon;
+            if(ledon) {
+                button.setText("已经全部点亮");
+                checkBoxLed1.setChecked(true);
+                checkBoxLed2.setChecked(true);
+            }
+            else{
+                button.setText("已经全部熄灭");
+                checkBoxLed1.setChecked(false);
+                checkBoxLed2.setChecked(false);
+            }
+        }
+    }
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.LED1:
+                if (checked){
+                    Toast.makeText(getApplicationContext(),"LED1 已点亮",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"LED1 已熄灭",Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.LED2:
+                if (checked){
+                    Toast.makeText(getApplicationContext(),"LED2 已点亮",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"LED2 已熄灭",Toast.LENGTH_LONG).show();
+                }
+                break;
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.BUTTON);
-        //匿名类写法
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                ledon =!ledon;
-                if(ledon)
-                    button.setText("ALL OFF");
-                else
-                    button.setText("ALL ON");
-            }
-        });
+        checkBoxLed1 = (CheckBox)  findViewById(R.id.LED1);
+        checkBoxLed2 = (CheckBox)  findViewById(R.id.LED2);
 
-
+        button.setOnClickListener(new myButtonListener());
     }
 }
