977714de878b7cf653b1ae002b66e2ad7782f586
wangyanxiang2 <wangyanxiang2@hisense.com>
2019-07-17 14:31:44 +0800

    可以使用服务打开led 5.1系统

--- study-android-demo/app/src/main/java/com/user/study_android_demo/MainActivity.java
+++ study-android-demo/app/src/main/java/com/user/study_android_demo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.user.study_android_demo;
 
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.menu.ActionMenuItemView;
@@ -7,14 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
-import com.user.hardcontrol.*;
+import android.os.ILedService;
+import android.os.ServiceManager;
 public class MainActivity extends AppCompatActivity {
 
     private boolean ledon = false;
     private Button button =null ;
     private CheckBox checkBoxLed1 = null;
     private CheckBox checkBoxLed2 = null;
-
+    private ILedService iLedService = null;
     class myButtonListener implements View.OnClickListener
     {
         @Override
@@ -25,15 +27,23 @@ public class MainActivity extends AppCompatActivity {
                 button.setText("已经全部点亮");
                 checkBoxLed1.setChecked(true);
                 checkBoxLed2.setChecked(true);
-                HardControl.ledCtrl(0,1);
-                HardControl.ledCtrl(1,1);
+                try {
+                    iLedService.ledCtrl(0,1);
+                    iLedService.ledCtrl(1,1);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
             }
             else{
                 button.setText("已经全部熄灭");
                 checkBoxLed1.setChecked(false);
                 checkBoxLed2.setChecked(false);
-                HardControl.ledCtrl(0,0);
-                HardControl.ledCtrl(1,0);
+                try {
+                    iLedService.ledCtrl(0,0);
+                    iLedService.ledCtrl(1,0);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -42,28 +52,32 @@ public class MainActivity extends AppCompatActivity {
         boolean checked = ((CheckBox) view).isChecked();
 
         // Check which checkbox was clicked
-        switch(view.getId()) {
-            case R.id.LED1:
-                if (checked){
-                    HardControl.ledCtrl(0,1);
-                    Toast.makeText(getApplicationContext(),"LED1 已点亮",Toast.LENGTH_LONG).show();
-                }
-                else{
-                    HardControl.ledCtrl(0,0);
-                    Toast.makeText(getApplicationContext(),"LED1 已熄灭",Toast.LENGTH_LONG).show();
-                }
-                break;
-            case R.id.LED2:
-                if (checked){
-                    HardControl.ledCtrl(1,1);
-                    Toast.makeText(getApplicationContext(),"LED2 已点亮",Toast.LENGTH_LONG).show();
-                }
-                else{
-                    HardControl.ledCtrl(1,0);
-                    Toast.makeText(getApplicationContext(),"LED2 已熄灭",Toast.LENGTH_LONG).show();
-                }
-                break;
+        try {
+            switch(view.getId()) {
+                case R.id.LED1:
+                    if (checked){
+                        iLedService.ledCtrl(0,1);
+                        Toast.makeText(getApplicationContext(),"LED1 已点亮",Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        iLedService.ledCtrl(0,0);
+                        Toast.makeText(getApplicationContext(),"LED1 已熄灭",Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                case R.id.LED2:
+                    if (checked){
+                        iLedService.ledCtrl(1,1);
+                        Toast.makeText(getApplicationContext(),"LED2 已点亮",Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        iLedService.ledCtrl(1,0);
+                        Toast.makeText(getApplicationContext(),"LED2 已熄灭",Toast.LENGTH_LONG).show();
+                    }
+                    break;
 
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
         }
     }
     @Override
@@ -72,8 +86,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.BUTTON);
+        iLedService =  ILedService.Stub.asInterface(
+                ServiceManager.getService("led"));
 
-        HardControl.ledOpen();
         checkBoxLed1 = (CheckBox)  findViewById(R.id.LED1);
         checkBoxLed2 = (CheckBox)  findViewById(R.id.LED2);
 
